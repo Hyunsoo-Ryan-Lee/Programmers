00df678a2ab3b03c4f205e8a83268e8547241fe5
Hyunsoo-Ryan-Lee <83285291+Hyunsoo-Ryan-Lee@users.noreply.github.com>
2021-06-12 23:28:11 +0900

    DFS/BFS

--- 모의고사,소수찾기,숫자카드(백준).py
+++ 모의고사,소수찾기,숫자카드(백준).py
@@ -64,4 +64,12 @@ print(solution(a))
 
 from collections import Counter
 
-print(Counter('is this the real life'))
+# print(Counter('is this the real life'))
+# Counter({' ': 4, 'i': 3, 'e': 3, 's': 2, 't': 2, 'h': 2, 'l': 2, 'r': 1, 'a': 1, 'f': 1})
+n = int(input())
+a = list(map(int,input().split(' ')))
+m = int(input())
+b = list(map(int,input().split(' ')))
+card = Counter(a) # 요 구문을 for문의 print안에 넣어버리면 시간 초과가 됨.
+for i in b:
+    print(card[i],end=' ')
